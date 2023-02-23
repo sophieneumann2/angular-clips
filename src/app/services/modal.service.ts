6b1380fa9cd5ece89d8932e1d0c3353c0e12ff57
Sophie Neumann <sophieneumann@ymail.com>
2023-02-23 10:46:21 +0100

    added id handling for modals

--- src/app/services/modal.service.ts
+++ src/app/services/modal.service.ts
@@ -1,18 +1,31 @@
 import { Injectable } from '@angular/core';
 
+interface IModal {
+  id: string;
+  visible: boolean;
+}
+
 @Injectable({
   providedIn: 'root',
 })
 export class ModalService {
-  private visible = false;
+  private modals: IModal[] = [];
 
   constructor() {}
 
-  isModalVisible() {
-    return this.visible;
+  register(id: string) {
+    this.modals.push({ id, visible: false });
   }
 
-  toggleModal() {
-    this.visible = !this.visible;
+  isModalVisible(id: string): boolean {
+    return !!this.modals.find((element) => element.id === id)?.visible;
+  }
+
+  toggleModal(id: string) {
+    const modal = this.modals.find((element) => element.id === id);
+
+    if (modal) {
+      modal.visible = !modal.visible;
+    }
   }
 }
