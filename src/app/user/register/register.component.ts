7a0db2d8f2261a94568b879c9ff54a1f57741fa3
Sophie Neumann <sophieneumann@ymail.com>
2023-02-25 17:35:58 +0100

    added input component

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
+  /* values are defined here and not inside new FormGroup to avoid that they are typed AbstractControl. */
+  name = new FormControl('', [Validators.required, Validators.minLength(3)]);
+  email = new FormControl('');
+  age = new FormControl('');
+  password = new FormControl('');
+  confirmPassword = new FormControl('');
+  phoneNumber = new FormControl('');
 
+  registerForm = new FormGroup({
+    name: this.name,
+    email: this.email,
+    age: this.age,
+    password: this.password,
+    confirmPassword: this.confirmPassword,
+    phoneNumber: this.phoneNumber,
+  });
 }
