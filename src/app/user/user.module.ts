9cad880d6f1f6aa72c27a73ff6cc765ce3a2a151
Sophie Neumann <sophieneumann@ymail.com>
2023-02-25 13:29:53 +0100

    created new components (forms), refactored login and register form

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { AuthModalComponent } from './auth-modal/auth-modal.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
-  declarations: [AuthModalComponent],
+  declarations: [AuthModalComponent, LoginComponent, RegisterComponent],
   imports: [CommonModule, SharedModule],
   exports: [AuthModalComponent],
 })
