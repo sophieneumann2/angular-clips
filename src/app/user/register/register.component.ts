d6f05eeb137b97bad453c9daac55abf5ea646ea2
Sophie Neumann <sophieneumann@ymail.com>
2023-02-25 19:20:11 +0100

    update form validation

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -9,10 +9,17 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class RegisterComponent {
   /* values are defined here and not inside new FormGroup to avoid that they are typed AbstractControl. */
   name = new FormControl('', [Validators.required, Validators.minLength(3)]);
-  email = new FormControl('');
-  age = new FormControl('');
-  password = new FormControl('');
-  confirmPassword = new FormControl('');
+  email = new FormControl('', [Validators.required, Validators.email]);
+  age = new FormControl('', [
+    Validators.required,
+    Validators.min(18),
+    Validators.max(120),
+  ]);
+  password = new FormControl('', [
+    Validators.required,
+    Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm),
+  ]);
+  confirmPassword = new FormControl('', [Validators.required]);
   phoneNumber = new FormControl('');
 
   registerForm = new FormGroup({
