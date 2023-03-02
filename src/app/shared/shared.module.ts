60c6ad99730011e4b6f429f180048e9f98eb08ad
Sophie Neumann <83170420+sophieneumann2@users.noreply.github.com>
2023-03-02 08:44:57 +0100

    Feature/reactive forms (#3)
    * created new components (forms), refactored login and register form
    
    * added input component
    
    * update form validation
    
    * applied input masking
    
    * handled disabled button, added Alert component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ModalComponent } from './modal/modal.component';
+import { ReactiveFormsModule } from '@angular/forms';
 import { TabsContainerComponent } from './tabs-container/tabs-container.component';
 import { TabComponent } from './tab/tab.component';
+import { InputComponent } from './input/input.component';
+import { provideEnvironmentNgxMask, NgxMaskDirective } from 'ngx-mask';
+import { AlertComponent } from './alert/alert.component';
 //import { ModalService } from '../services/modal.service';
 
 @NgModule({
-  declarations: [ModalComponent, TabsContainerComponent, TabComponent],
-  imports: [CommonModule],
-  exports: [ModalComponent, TabsContainerComponent, TabComponent],
-  //providers: [ModalService]
+  declarations: [
+    ModalComponent,
+    TabsContainerComponent,
+    TabComponent,
+    InputComponent,
+    AlertComponent,
+  ],
+  imports: [CommonModule, ReactiveFormsModule, NgxMaskDirective],
+  exports: [
+    ModalComponent,
+    TabsContainerComponent,
+    TabComponent,
+    InputComponent,
+    AlertComponent,
+  ],
+  providers: [provideEnvironmentNgxMask()],
 })
 export class SharedModule {}
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from '../shared/shared.module';
 import { AuthModalComponent } from './auth-modal/auth-modal.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
-  declarations: [AuthModalComponent],
-  imports: [CommonModule, SharedModule],
+  declarations: [AuthModalComponent, LoginComponent, RegisterComponent],
+  imports: [CommonModule, SharedModule, ReactiveFormsModule],
   exports: [AuthModalComponent],
 })
 export class UserModule {}
